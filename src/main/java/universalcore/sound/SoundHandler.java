4c4c022e19dd2ff928a7a53aba8236c2ee3e201c
SeamusFD <armstrongmediahome@icloud.com>
2014-05-18 20:43:01 -0500

    Added Import statements and a Client side event

--- src/main/java/universalcore/sound/SoundHandler.java
+++ src/main/java/universalcore/sound/SoundHandler.java
@@ -1,8 +1,15 @@
 package main.java.universalcore.sound;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+import net.minecraft.client.audio.ISound;
+import net.minecraft.util.ResourceLocation;
+
 /**
  * Created by SeamusFD on 5/18/14.
  * All rights belong to me!!
  */
-public class SoundHandler {
+@SideOnly(Side.CLIENT)
+public class SoundHandler implements ISound {
 }

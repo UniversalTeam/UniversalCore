9b32ad6345a8da53b08fbc84e8c57055662ccf4e
big_Xplosion <bigxplosion44@gmail.com>
2014-05-31 20:52:49 +0200

    Some small changes to the build.gradle file

--- src/main/java/universalteam/universalcore/libs/ReferenceCore.java
+++ src/main/java/universalteam/universalcore/libs/ReferenceCore.java
@@ -4,5 +4,5 @@ public class ReferenceCore
 {
 	public static final String MODID = "UniversalCore";
 	public static final String MODNAME = "UniversalCore";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "@VERSION@";
 }

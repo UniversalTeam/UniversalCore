970deae50a3962d81cc98c6798ccd00a348a2d45
big_Xplosion <bigxplosion44@gmail.com>
2014-06-18 22:23:49 +0200

    Renamed RomanNumber to RomanNumeral in favor of @warriordog

--- src/main/java/universalteam/universalcore/utils/math/RomanNumeral.java
+++ src/main/java/universalteam/universalcore/utils/math/RomanNumeral.java
@@ -1,22 +1,22 @@
 package universalteam.universalcore.utils.math;
 
-public class RomanNumber
+public class RomanNumeral
 {
 	protected static final int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 	protected static final String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
-	public static String toRomanNumber(int number)
+	public static String toRomanNumeral(int number)
 	{
 		StringBuilder builder = new StringBuilder();
 		int remaining = number;
 
 		for (int i = 0; i < values.length; ++i)
-			remaining = appendRomanNumber(remaining, values[i], symbols[i], builder);
+			remaining = appendRomanNumeral(remaining, values[i], symbols[i], builder);
 
 		return builder.toString();
 	}
 
-	protected static int appendRomanNumber(int remaining, int value, String symbol, StringBuilder builder)
+	protected static int appendRomanNumeral(int remaining, int value, String symbol, StringBuilder builder)
 	{
 		while (remaining >= value)
 		{

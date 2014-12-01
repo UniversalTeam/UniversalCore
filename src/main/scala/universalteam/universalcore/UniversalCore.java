8f011bc484d7243d02f98929f0cd5d235b78002c
big_Xplosion <bigxplosion44@gmail.com>
2014-12-01 20:48:45 +0100

    Added basic mod files

---
+++ src/main/scala/universalteam/universalcore/UniversalCore.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2014, big_Xplosion and the UniversalTeam
+ *
+ * UniversalTeam's mods are distributed under the terms of GNU LGPL v3.0
+ * Please check the contents of the license located in
+ * https://www.gnu.org/licenses/lgpl.html
+ *
+ * @author big_Xplosion
+ */
+package universalteam.universalcore;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import universalteam.universalcore.lib.Reference;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION)
+public class UniversalCore
+{
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+
+	}
+
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+
+	}
+
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		
+	}
+}
---
+++ src/main/scala/universalteam/universalcore/lib/Reference.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2014, big_Xplosion and the UniversalTeam
+ *
+ * UniversalTeam's mods are distributed under the terms of GNU LGPL v3.0
+ * Please check the contents of the license located in
+ * https://www.gnu.org/licenses/lgpl.html
+ *
+ * @author big_Xplosion
+ */
+package universalteam.universalcore.lib;
+
+public class Reference
+{
+	public static final String MOD_ID = "UniversalCore";
+	public static final String MOD_NAME = "UniversalCore";
+	public static final String MOD_VERSION = "@VERSION@";
+}
